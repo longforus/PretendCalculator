74dbd0671644f99ea546c07f7d669062b946e3e2
longforus <longforus@126.com>
2017-05-17 20:32:24 +0800

    add image

---
+++ app/src/main/java/cn/studyjams/s2/sj0265/yangxiqiang/MyApplication.kt
@@ -0,0 +1,14 @@
+package cn.studyjams.s2.sj0265.yangxiqiang
+
+import android.app.Application
+
+/**
+ * Created by XQ Yang on 2017/5/17  20:05.
+ * Description :
+ */
+class MyApplication:Application(){
+	override fun onCreate() {
+		super.onCreate()
+		
+	}
+}
---
+++ app/src/main/java/cn/studyjams/s2/sj0265/yangxiqiang/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package cn.studyjams.s2.sj0265.yangxiqiang.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import cn.studyjams.s2.sj0265.yangxiqiang.R
+
+class MainActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState : Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
